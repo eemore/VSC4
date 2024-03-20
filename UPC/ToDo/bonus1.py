3fe7ed9de20f7eb821fe38c934a197444522ad62
Efe Emore <eemore@gmail.com>
2024-03-20 16:00:49 -0300

    D72

--- UPC/ToDo/bonus1.py
+++ UPC/ToDo/bonus1.py
@@ -1,6 +1,5 @@
-text = input("Enter a title")
+filenames = ['1. Raw Data.txt', '2. Reports.txt', '3. Presentations.txt']
 
-length = len(text)
-
-
-print("The length of the title is:",length)
+for filename in filenames:
+    filename = filename.replace('.','-',1)
+    print(filename)
--- UPC/ToDo/main.py
+++ UPC/ToDo/main.py
@@ -1,25 +1,35 @@
 #print("Enter a todo:")
+filename='files/todo.txt'
+file=open(filename,'r')
+todos = file.readlines()
+file.close()
 
 
-todos = []
 while True:
-    user_choice = input("Enter add, show, edit or exit:")
+    user_choice = input("Enter add, show, edit, save or exit:")
     user_choice = user_choice.strip().lower()
 
     match(user_choice):
         case 'add'| '':
-            todo = input("Enter a todo:")
+            todo = input("Enter a todo:")+'\n'
             todos.append(todo)
         case 'show' | 'display':
             #print(todos)
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                print(f'{index+1}.{item.replace("\n","")}')
         case 'edit':
             n=int(input("Item number to Edit. 0 to exit: "))
             if n==0:
                 break
-            todo=input("enter replacement for '"+ todos[n-1]+"': ")
-            todos[n-1]=todo
+            if n > len(todos):
+                print('Invalid Number Entered')  
+            else:              
+                todo=input("enter replacement for '"+ todos[n-1]+"': ")
+                todos[n-1]=todo
+        case 'save':
+            file=open(filename,'w')
+            file.writelines(todos)
+            file.close()
         case 'exit':
             break
         case _:
