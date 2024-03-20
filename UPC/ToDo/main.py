fa27d8206cfb6397585ba80a0c1c997cf482f37b
Efe Emore <eemore@gmail.com>
2024-03-20 08:37:46 -0300

    What did I change

--- UPC/ToDo/main.py
+++ UPC/ToDo/main.py
@@ -3,17 +3,29 @@
 
 todos = []
 while True:
-    user_choice = input("Enter add, show or exit:")
+    user_choice = input("Enter add, show, edit or exit:")
+    user_choice = user_choice.strip().lower()
+
     match(user_choice):
-        case 'add':
+        case 'add'| '':
             todo = input("Enter a todo:")
             todos.append(todo)
-        case 'show':
+        case 'show' | 'display':
             #print(todos)
             for item in todos:
                 print(item)
+        case 'edit':
+            n=int(input("Item number to Edit. 0 to exit: "))
+            if n==0:
+                break
+            todo=input("enter replacement for '"+ todos[n-1]+"': ")
+            todos[n-1]=todo
         case 'exit':
             break
+        case _:
+            print("Hey you entered an unknown command")
+    
+
 
 print("Bye")
 
